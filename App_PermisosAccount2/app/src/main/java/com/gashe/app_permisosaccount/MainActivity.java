ddbc5ddb820381600773872ff55735c44d1b2263
cice <cice@aula8PovMacN09.local>
2017-03-04 13:35:26 +0100

    subactivies app

--- App_PermisosAccount2/app/src/main/java/com/gashe/app_permisosaccount/MainActivity.java
+++ App_PermisosAccount2/app/src/main/java/com/gashe/app_permisosaccount/MainActivity.java
@@ -86,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
                 final SharedPreferences prefs = getSharedPreferences("pref", Context.MODE_PRIVATE);
                 String cuentasString = prefs.getString("cuentas", "");
                 String [] string_cuentas = cuentasString.split(",");
